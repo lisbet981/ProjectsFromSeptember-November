93af9bd5bd4dbec40ff7a3a8ef7653a93884f294
lisbet981 <38430@ocvts.org>
2025-11-25 13:44:02 +0000

    SimpleCalculator

---
+++ SimpleCalculator.java
@@ -0,0 +1,52 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        // This will be the first thing that I want the user to see when entering the
+        // terminal to basicaly know what to do
+        System.out.println("Hey there, Welcome To the place where I will be able to solve all of your problems with number");
+    }
+
+}
+// import java.util.*; 
+// public class SimpleCalculatorStarterCode{ 
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);  
+//         System.out.print("Enter name: ");
+//         String name = input.nextLine();
+//         System.out.print("Welcome " + name );
+
+//         // The numbers being inputing 
+//         System.out.print(" Enter the First number to your equation, *only numbers 1-10*?");
+//        double number1 = input.nextDouble();
+//         System.out.print(" Enter the Second number to your equation, *only numbers 1-10*?");
+//        double number2 = input.nextDouble();
+
+//         input.nextLine();
+
+//         System.out.print("How would you like to solve these two numbers equation, (ex: +, -, *, +)?");
+//         String operation = input.nextLine();
+//         char operation
+//         double results;
+
+//   if (operation == '+') {
+//             result = num1 + num2;
+//             System.out.println("Result: " + result);
+//         } else if (operation == '-') {
+//             result = num1 - num2;
+//             System.out.println("Result: " + result);
+
+
+
+
+// Math Example
+/**
+ * 
+public class MathExample {
+public static void main(String[] args) {
+ double myRandomNum = Math.random(); // generates a number between 0.0 and 1.0
+ System.out.println("\nGenerates a random number from 0.0 to 1.0: " + myRandomNum);
+
+ int randomNum = (int)(Math.random() * 100) + 1; // gives a number from 1 to 100
+ System.out.println("\nGenerates a random number from 1 to 100: " + randomNum);
+ }
+ }
+ */
