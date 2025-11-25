897c8191e8637b9568ec63ef13f3982a95825606
lisbet981 <38430@ocvts.org>
2025-11-25 14:01:25 +0000

    ChallengesTurnedIn

---
+++ ChallengesTurnedIn.java
@@ -0,0 +1,113 @@
+// import java.util.LinkedList;
+// import java.util.ArrayList;
+
+// public class Examples4 {
+//     public static void main(String[] args) {
+
+//CHALLENGE ONE:
+// int[] arr = new int[3];  "this is wrong in the form that if place "new int[3]" than you shouldn't have "arr[3] = 40" you can only have it in one place --NOT BOTH-- "
+// arr[0] = 10;
+// arr[1] = 20;
+// arr[2] = 30;
+// arr[3] = 40;
+// for(int i = 0; i <= arr.length; i++) {  " in this line as well you have to take the "=" from "<=" "
+
+// System.out.println(arr(i));
+// }
+
+
+
+//CHALLENGE TWO:
+// int count =1;
+// while( count<=3){
+//     System.out.print("Hello" + count);
+// count+++
+// }
+
+
+
+// for(int k =0; )same thing as the one in the bottom
+
+// int arrow;
+// for(arrow = 0; arrow <=20; arrow= 5)
+// {
+//      System.out.print(arrow);
+// }
+
+// byte b= 120;
+// double d = b; //the double is to understand that double just adds ".0"
+//  System.out.println(d);
+
+
+// String s1 ="abc";
+// String s2 = "123";
+//  System.out.print(s1.concat(s2));   where ever you see ".concat" theres no space 
+
+
+//SYSTEM OF DIVISION
+// int a = 12;
+// int b = 5;
+//  System.out.println( a % b);
+
+
+// ArrayList<String> tasks = new ArrayList<>();
+// tasks.add("Read");       "This would cancel out" because this is 0
+// tasks.add("Write");       This is " 1 "
+// tasks.remove(0);          This is "2"
+// System.out.println(tasks);  "The Answer would be (Write)" because...
+
+//CHALLENGE 7:
+// int x = 200;
+// long l = x;
+// float 
+
+//CHALLENGE 8:
+// LinkedList<String> letters = new LinkedList<>();
+// letters.add("X");
+// letters.add("Y");
+// System.out.println(letters.indexOf("Y")); 
+
+
+// CHALLENGE 9:
+// int ring;
+// for(ring = 0; ring <= 5; ring++)
+// System.out.println(ring);
+
+
+//Challenge #10
+// int crystal;
+// while (crystal<= 3;){
+//     double result = (double)crystal / 2;
+// }
+
+//Challenge #11
+// System.out.println(Math.pow(2, 3)); "out put comes out to 8.0"
+
+//Challenge #12
+//   boolean flag = true;
+// System.out.println(!flag); //out put will be "False"  *"!" means "NOT"
+
+
+//Challenge #13
+// boolean a = false;
+// boolean b = true;
+// System.out.println(a || b); output will be "true" when ever one part is true than it will come out true
+
+//Challenge #13
+// int a = 10;
+// int b = 5;
+// boolean result = (a % b == 0);
+// System.out.println(result); //"output will be (true) bc 5 can go into 10 with no number remainding "
+
+// Challenge #15
+// float f = 7.99f;
+// int n = (int) f;
+// System.out.println(n); "output wil be (7) it doesn't run up "
+    // }
+// }
+
+
+
+
+
+
